bcec008720a72afd9c1ed9151d29cccef785c029
paiigak <66020711+paiigak@users.noreply.github.com>
2023-07-13 15:14:48 +0700

    up actions

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,8 @@ export const numbers = [1,9,7,1,4]
 
 
 export const min = function (num: number[]): number {
-  let min = 10
+  if (!num.length) throw Error("numbers was empty");
+  let min = num[0]
   for(let i=0; i<num.length; i++) {
     if(num[i] < min)
     min = num[i]
@@ -17,7 +18,8 @@ export const min = function (num: number[]): number {
 console.log(min(numbers))
 
 export const max = function (num: number[]): number {
-  let max = 1
+  if (!num.length) throw Error("numbers was empty");
+  let max = num[0]
   for(let i=0; i<num.length; i++) {
     if(num[i] > max)
     max= num[i]
@@ -29,6 +31,7 @@ console.log(max(numbers))
 
 
 export const avg = function (num: number[]): number {
+  if (!num.length) throw Error("numbers was empty");
   const avg = num.reduce((acc,cur) => acc+cur,0) 
   return avg/num.length
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,6 +1,7 @@
+import { describe } from "node:test";
 import { min, max, avg } from "../src";
 
-describe("min", () => {
+describe("test min", () => {
   it("min should be min", () => {
     expect(min([1,12,5,6,7,1.5,9]))
   })
@@ -44,4 +45,90 @@ describe("avg", () => {
     expect(avg([-1,-1.5,-5,-6,-7,-0.5,Number(-9n)]))
   })
 })
+
+// test art
+describe("test max", () => {
+  test("empty array should throw error", () => {
+    expect(() => {
+      max([]);
+    }).toThrow();
+
+    expect(() => {
+      max([]);
+    }).toThrowError("numbers was empty");
+  });
+
+  test("array with single element should return that element", () => {
+    expect(max([1])).toEqual(1);
+  });
+
+  test("should return max in array", () => {
+    const tests: [number[], number][] = [
+      [[1, 2, 3], 3],
+      [[-1, -2], -1],
+      [[7, 8, 1], 8],
+    ];
+
+    tests.forEach((test) => {
+      const [input, expected] = test;
+      expect(max(input)).toEqual(expected);
+    });
+  });
+});
+
+describe("test min", () => {
+  test("empty array should throw error", () => {
+    expect(() => {
+      min([]);
+    }).toThrow();
+    expect(() => {
+      min([]);
+    }).toThrowError("numbers was empty");
+  });
+
+  test("array with single element should return that element", () => {
+    expect(min([-1])).toEqual(-1);
+  });
+
+  test("should return max in array", () => {
+    const tests: [number[], number][] = [
+      [[1, 2, 3], 1],
+      [[-1, -2], -2],
+      [[7, 8, 1], 1],
+    ];
+
+    tests.forEach((test) => {
+      const [input, expected] = test;
+      expect(min(input)).toEqual(expected);
+    });
+  });
+});
+
+describe("test avg", () => {
+  test("empty array should throw error", () => {
+    expect(() => {
+      avg([]);
+    }).toThrow();
+    expect(() => {
+      avg([]);
+    }).toThrowError("numbers was empty");
+  });
+
+  test("array with single element should return that element", () => {
+    expect(avg([33])).toEqual(33);
+  });
+
+  test("should return max in array", () => {
+    const tests: [number[], number][] = [
+      [[1, 2, 3], 2],
+      [[-1, -2], -1.5],
+      [[-10, 0, 10], 0],
+    ];
+
+    tests.forEach((test) => {
+      const [input, expected] = test;
+      expect(avg(input)).toEqual(expected);
+    });
+  });
+});
 
